3378c8fc7e17e3637dc7bbc79a5d2458ed579bf3
Markus Jungbluth <markus228@users.noreply.github.com>
2017-02-22 13:46:09 +0100

    Typo

--- curve.py
+++ curve.py
@@ -1,7 +1,7 @@
 from fractions import Fraction
 
 """
-This is a very basic iplementation of elliptic curves.
+This is a very basic implementation of elliptic curves.
 It allows to define a Curve(a, b, p) with E: y^2 = x^3 + a*x + b mod p
 The point class allows to operate on this curve and implements addition and multiplication.
 
